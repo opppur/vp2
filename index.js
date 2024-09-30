9ea1f55632daec96463e79f7691c5e4bd4a8dafa
Jonas Oliveira <opppur@users.noreply.github.com>
2024-09-30 13:13:59 +0300

    Kasutaja sisendi tekstifaili kirjutamine lisatud

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ const express = require("express");
 const app = express();
 const fs = require("fs");
 const dateTime = require("./dateTime");
+//et saada kõik päringust kätte
+const bodyparser = require("body-parser");
 
 
 //	Määran view mootori
 app.set("view engine", "ejs");
 //määran jagatavate avalike failide kausta
 app.use(express.static("public"));
+//kasutane body-parserit päringute parsimiseks (kui ainult tekst, siis false, kui ka pildid jms, siis true)
+app.use(bodyparser.urlencoded({extended: false}));
 
 app.get("/", (req, res)=>{
 	//res.send("Express läks käima!"); 
@@ -29,8 +33,36 @@ app.get("/vanasonad",(req, res)=>{
 		}
 		else {
 			folkWisdom = data.split(";");
-			res.render("justlist", {h2: "Vanasõnad", listData: folkWisdom});
+			res.render("justlist", {h2: "Vanasõnad", listData: folkWisdom
+			});
 		}
-	})
+	});
 });
+
+app.get("/regvisit", (req, res)=>{
+	res.render("regvisit");
+});
+
+app.post("/regvisit", (req, res)=>{
+	//console.log(req.body);
+	//avan txt faili selliselt, et kui seda pole olemas, luuakse
+	fs.open("public/textfiles/log.txt", "a", (err, file) =>{
+		if(err){
+			throw err;
+		}
+		else {
+			fs.appendFile("public/textfiles/log.txt", req.body.firstNameInput + " " + req.body.lastNameInput + ";", (err)=>{
+				if(err){
+					throw err;
+				}
+				else {
+					console.log("faili kirjutati!");
+					res.render("regvisit");
+				}
+			});
+		}
+	});
+
+});
+
 app.listen(5207);
