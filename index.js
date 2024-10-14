55bddcf5ca5118a098d35d61239b337b91388299
Jonas Oliveira <opppur@users.noreply.github.com>
2024-10-14 13:34:16 +0300

    Lisatud andmebaasiühendus

--- index.js
+++ index.js
@@ -1,31 +1,42 @@
 const express = require("express");
-const app = express();
-const fs = require("fs");
 const dateTime = require("./dateTime");
+const fs = require("fs");
 //et saada kõik päringust kätte
 const bodyparser = require("body-parser");
+//andmebaasi andmed
+const dbInfo = require("../../vp2024config");
+//andmebaasiga suhtlemine
+const mysql = require("mysql2");
 
+const app = express();
 
-//	Määran view mootori
+//määran view mootori
 app.set("view engine", "ejs");
-//määran jagatavate avalike failide kausta
+//määran jagatavate, avalike failide kausta
 app.use(express.static("public"));
-//kasutane body-parserit päringute parsimiseks (kui ainult tekst, siis false, kui ka pildid jms, siis true)
+//kasutame body-parserit päringute parsimiseks (kui ainult tekst, siis false, kui ka pildid, siis true)
 app.use(bodyparser.urlencoded({extended: false}));
 
-app.get("/", (req, res)=>{
-	//res.send("Express läks käima!"); 
-	res.render("index");
-}); 
-
-app.get("/timenow",(req, res)=>{
-	const weekdayNow = dateTime.weekDayEt();
-	const dateNow = dateTime.dateFormattedEt();
-	const timeNow = dateTime.timeFormattedEt();
-	res.render("timenow", {nowWD: weekdayNow, nowD: dateNow, nowT: timeNow});
+//andmebaasi ühendus
+const conn = mysql.createConnection({
+	host: dbInfo.configData.host,
+	user: dbInfo.configData.user,
+	password: dbInfo.configData.passWord,
+	database: dbInfo.configData.dataBase
 });
 
-app.get("/vanasonad",(req, res)=>{
+app.get("/", (req, res)=>{
+	//res.send("Express läks käima!!")
+	res.render("index");
+});
+
+app.get("/timenow", (req, res)=>{
+	const dateNow = dateTime.dateFormattedEt();
+	const timeNow = dateTime.timeFormattedEt();
+	res.render("timenow", {nowD: dateNow, nowT: timeNow});
+});
+
+app.get("/vanasonad", (req, res)=>{
 	let folkWisdom = [];
 	fs.readFile("public/textfiles/vanasonad.txt", "utf8", (err, data)=>{
 		if(err){
@@ -33,8 +44,7 @@ app.get("/vanasonad",(req, res)=>{
 		}
 		else {
 			folkWisdom = data.split(";");
-			res.render("justlist", {h2: "Vanasõnad", listData: folkWisdom
-			});
+			res.render("justlist", {h2: "Vanasõnad", listData: folkWisdom});
 		}
 	});
 });
@@ -44,25 +54,94 @@ app.get("/regvisit", (req, res)=>{
 });
 
 app.post("/regvisit", (req, res)=>{
-	//console.log(req.body);
-	//avan txt faili selliselt, et kui seda pole olemas, luuakse
-	fs.open("public/textfiles/log.txt", "a", (err, file) =>{
+	const dateNow = dateTime.dateFormattedEt();
+	const timeNow = dateTime.timeFormattedEt();
+	//console.log(reg.body);
+	//avan .txt faili selliselt, et kui seda ei eksisteeri, see luuakse
+	fs.open("public/textfiles/log.txt", "a", (err, file)=> {
 		if(err){
 			throw err;
 		}
 		else {
-			fs.appendFile("public/textfiles/log.txt", req.body.firstNameInput + " " + req.body.lastNameInput + ";", (err)=>{
+			fs.appendFile("public/textfiles/log.txt", ";" + req.body.firstNameInput + " " + req.body.lastNameInput + " " + dateNow + ", " + timeNow, (err)=>{
 				if(err){
 					throw err;
 				}
 				else {
-					console.log("faili kirjutati!");
+					console.log("Faili kirjutati!");
 					res.render("regvisit");
 				}
 			});
 		}
 	});
+});
 
+app.get("/visitlog", (req, res)=>{
+	let visitorName = [];
+	fs.readFile("public/textfiles/log.txt", "utf8", (err, data)=>{
+		if(err){
+			throw err;
+		}
+		else {
+			visitorName = data.split(";");
+			res.render("visitlog", {h2: "Külalised", listData: visitorName});
+		}
+	});
+});
+
+app.get("/regvisitdb", (req, res)=>{
+	let notice = "";
+	let firstName = "";
+	let lastName = "";
+	res.render("regvisitdb", {notice: notice, firstName : firstName, lastName : lastName});
+});
+
+app.post("/regvisitdb", (req, res)=>{
+	let notice = "";
+	let firstName = "";
+	let lastName = "";
+	//kontrollin kas vajalikud andmed on olemas
+	if(!req.body.firstNameInput || !req.body.lastNameInput){
+		//console.log("Osa andmeid puudu");
+		notice = "Osa andmeid on puudu";
+		firstName = req.body.firstNameInput;
+		lastName = req.body.lastNameInput;
+		res.render("regvisitdb", {notice: notice, firstName : firstName, lastName : lastName});
+	}
+	else {
+		let sqlReq = "INSERT INTO vp2visitlog (first_name, last_name) VALUES(?,?)";
+		conn.query(sqlReq, [req.body.firstNameInput, req.body.lastNameInput], (err, sqlRes)=>{
+			if(err) {
+				notice = "Andmeid ei salvestatud";
+				res.render("regvisitdb", {notice: notice});
+				throw err;
+			}
+			else {
+				notice = "Andmed salvestati!";
+				res.render("regvisitdb", {notice: notice});
+			}
+		});
+	}
+});
+
+app.get("/eestifilm", (req, res)=>{
+	res.render("eestifilm");
+});
+
+app.get("/eestifilm/tegelased", (req, res)=>{
+	//loon andmebaasipäringu
+	let sqlReq = "SELECT first_name, last_name, birth_date FROM person";
+	conn.query(sqlReq, (err,sqlRes)=>{
+		if(err){
+			res.render("tegelased", {persons: []});
+			//throw err;
+		}
+		else {
+			//console.log(sqlRes);
+			res.render("tegelased", {persons:sqlRes});
+		}
+	});
+	//res.render("tegelased");
 });
 
 app.listen(5207);
