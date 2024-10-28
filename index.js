b8b6b75f12e81f6bc0278b639218bee79bbdb5bb
Jonas Oliveira <opppur@users.noreply.github.com>
2024-10-28 13:39:22 +0200

    Galerii v1

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const bodyparser = require("body-parser");
 const dbInfo = require("../../vp2024config");
 //andmebaasiga suhtlemine
 const mysql = require("mysql2");
+//fotode üleslaadimiseks
+const multer = require("multer");
+//fotomanipulatsiooniks
+const sharp = require("sharp");
 
 const app = express();
 
@@ -15,7 +19,9 @@ app.set("view engine", "ejs");
 //määran jagatavate, avalike failide kausta
 app.use(express.static("public"));
 //kasutame body-parserit päringute parsimiseks (kui ainult tekst, siis false, kui ka pildid, siis true)
-app.use(bodyparser.urlencoded({extended: false}));
+app.use(bodyparser.urlencoded({extended: true}));
+//Seadistame fotode üleslaadimiseks vahevara (middleware), mis määrab kataloogi kuhu laetakse
+const upload = multer({dest: "./public/gallery/orig"});
 
 //andmebaasi ühendus
 const conn = mysql.createConnection({
@@ -93,7 +99,7 @@ app.get("/regvisitdb", (req, res)=>{
 	let notice = "";
 	let firstName = "";
 	let lastName = "";
-	res.render("regvisitdb", {notice: notice, firstName : firstName, lastName : lastName});
+	res.render("regvisitdb", {notice: notice, firstName: firstName, lastName: lastName});
 });
 
 app.post("/regvisitdb", (req, res)=>{
@@ -106,7 +112,7 @@ app.post("/regvisitdb", (req, res)=>{
 		notice = "Osa andmeid on puudu";
 		firstName = req.body.firstNameInput;
 		lastName = req.body.lastNameInput;
-		res.render("regvisitdb", {notice: notice, firstName : firstName, lastName : lastName});
+		res.render("regvisitdb", {notice: notice, firstName: firstName, lastName: lastName});
 	}
 	else {
 		let sqlReq = "INSERT INTO vp2visitlog (first_name, last_name) VALUES(?,?)";
@@ -142,6 +148,56 @@ app.get("/eestifilm/tegelased", (req, res)=>{
 		}
 	});
 	//res.render("tegelased");
+});
+
+app.get("/eestifilm/lisa", (req, res)=>{
+	res.render("addperson");
+});
+
+app.get("/photoupload", (req, res)=>{
+	res.render("photoupload")
+});
+
+app.post("/photoupload", upload.single("photoInput"), (req, res)=>{
+	console.log(req.body);
+	console.log(req.file);
+	const fileName = "vp_" + Date.now() + ".jpg";
+	fs.rename(req.file.path, req.file.destination + "/" + fileName, (err)=>{
+		console.log("Faili nime muutmise viga: " + err);
+	})
+	sharp(req.file.destination + "/" + fileName).resize(800,600).jpeg({quality: 90}).toFile("./public/gallery/normal/" + fileName);
+	sharp(req.file.destination + "/" + fileName).resize(100,100).jpeg({quality: 90}).toFile("./public/gallery/thumb/" + fileName);
+	//salvestame info andmebaasi
+	let sqlReq = "INSERT INTO vp2photos (file_name, orig_name, alt_text, privacy, user_id) VALUES(?,?,?,?,?)";
+	const userId = 1;
+	conn.query(sqlReq, [fileName, req.file.originalname, req.body.altInput, req.body.privacyInput, userId], (err, result)=>{
+		if(err){
+			throw(err);
+		}
+		else {
+			res.render("photoupload");
+		}
+	});
+	res.render("photoupload");
+});
+
+app.get("/gallery", (req, res)=>{
+	let sqlReq = "SELECT file_name, alt_text FROM vp2photos WHERE privacy = ? AND deleted IS NULL";
+	const privacy = 3;
+	let photoList = [];
+	conn.query(sqlReq, [privacy], (err, result)=>{
+		if(err){
+			throw err;
+		}
+		else {
+			console.log(result);
+			//result.foreach(photo => {
+				//photoList.push({href: "gallery/thumb/" + photo.file_name, alt: photo.alt_text});
+			//}
+			res.render("gallery", {listData: photoList});
+		};
+	});
+	res.render("gallery");
 });
 
 app.listen(5207);
